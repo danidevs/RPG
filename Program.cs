f7d3fd22cbe2336d346ff29fff7aa079053ac482
unknown <danielsk4te@hotmail.com>
2023-07-28 19:31:40 -0300

    initial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RPG_.Impostor.src.entidades;
+
+
+namespace RPG_.Impostor.src
+{
+
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Sindrome sindrome = new Sindrome("impostor", 50);
+            Estima  estima = new Estima ("Alto estima", 50);
+
+            Console.WriteLine(sindrome.Attack());
+            Console.WriteLine(estima.Attack(7));
+        }
+    }
+}
+
+
+
+
+
+
+
